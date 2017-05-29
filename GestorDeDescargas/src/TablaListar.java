bd0ceab5b183fec9be79b225e7e4e82b157fe134
aidass9 <aida9.soriano@gmail.com>
2017-05-29 21:25:00 +0200

    minims retocs 2

--- GestorDeDescargas/src/TablaListar.java
+++ GestorDeDescargas/src/TablaListar.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.GridLayout;
 
 import javax.swing.JLabel;
@@ -12,46 +13,31 @@ public class TablaListar extends JPanel {
 	JTable tablaListar;
 
 	TablaListar() {
-		/*JPanel panelTabla = new JPanel(new GridLayout(1, 1));
-		panelTabla.setBounds(0, 0, 500, 450);
 		
-		//setLayout(new GridLayout(1, 1));
 		
-		DefaultTableModel definicionTabla = new DefaultTableModel();
-		tablaListar = new JTable(definicionTabla);
-		
-			//Columnas de la tabla
-		definicionTabla.addColumn("Nombre");
-		definicionTabla.addColumn("Tamaño");
-		
-		tablaListar.setFillsViewportHeight(true);
-		tablaListar.getTableHeader();
-		
-		tablaListar.repaint();
-		tablaListar.revalidate();
-		
-		panelTabla.add(tablaListar);
-		add(panelTabla);*/
-		
+
 		setLayout(new GridLayout(1, 1));
-		setBounds(500, 150, 500, 250);
 		
 		DefaultTableModel definicionTabla = new DefaultTableModel();
 		tablaListar = new JTable(definicionTabla);
 
-		tablaListar.setSize(500, 250);
+		//tablaListar.setSize(500, 250);
 		
 			//Columnes de la tabla
 		definicionTabla.addColumn("Nombre");
 		definicionTabla.addColumn("Tamaño");
 		
-		tablaListar.setFillsViewportHeight(true);
-		tablaListar.getTableHeader();
+		/*Object[] objeto = new Object[2];
+		objeto[0] = "aida";
+		objeto[1] = 1;
+		definicionTabla.addRow(objeto);*/
 		
-		tablaListar.repaint();
-		tablaListar.revalidate();
+		tablaListar.setFillsViewportHeight(true);
 		
 		add(tablaListar);
+
+		tablaListar.repaint();
+		tablaListar.revalidate();
 		
 	}
 
